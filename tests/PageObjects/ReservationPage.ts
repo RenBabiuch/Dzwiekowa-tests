9c6a23f305bc2f5b3ac6c01c85074b03b3bff078
Renata <renbabiuch@gmail.com>
2025-09-01 20:25:51 +0200

    Revert "refactor PO and calendar-component"
    This reverts commit b76314d259186b026b41fb4d48d8be161232aee1.

--- tests/PageObjects/ReservationPage.ts
+++ tests/PageObjects/ReservationPage.ts
@@ -111,7 +111,7 @@ export class ReservationPagePO {
         const max = 9999999;
 
         const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-        return randomBeginningValue + String(randomNum).substring(0,1) + ' ' + String(randomNum).substring(1,4) + ' ' + String(randomNum).substring(4,7);
+        return randomBeginningValue + String(randomNum);
     }
 
     public async getStartDateValue() {
